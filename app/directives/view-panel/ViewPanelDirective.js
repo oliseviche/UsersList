29a205d6b9e4b29b3709493abcdaff711a7a9cb9
Eugene Olisevich <olisevich@hotmail.com>
2017-04-17 00:05:00 +0500

    Fix path

--- app/directives/view-panel/ViewPanelDirective.js
+++ app/directives/view-panel/ViewPanelDirective.js
@@ -4,7 +4,7 @@ define(['./ViewPanelController'], function(controller) {
 	return function viewPanel() {
 		return {
 			restrict: 'E',
-			templateUrl: '/app/directives/view-panel/view-panel.html',
+			templateUrl: './app/directives/view-panel/view-panel.html',
 			controller: controller,
 			controllerAs: 'ViewPanelCtrl',
 			replace: true,
